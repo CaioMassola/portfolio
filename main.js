539da5b275a803609f6e6a3842941c3760f89b4a
CaioMassola <caio.massola@outlook.com>
2022-04-14 16:56:27 -0300

    implements: button dark mode

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-function myFunction() {
-    var dark_mode = false;
-    console.log('aqui')
+function theme() {
+
+    var theme = document.getElementById("theme_1").innerHTML;
+
+
+    console.log(theme)
+
+    if (theme == 'dark_mode') {
+        theme = document.getElementById("theme_1").innerHTML = "light_mode";
+        theme = document.getElementById("theme_2").innerHTML = "light_mode";
+    } else {
+        theme = document.getElementById("theme_1").innerHTML = "dark_mode";
+        theme = document.getElementById("theme_2").innerHTML = "dark_mode";
+    }
 
 }
