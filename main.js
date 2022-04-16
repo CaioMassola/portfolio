6331a8736ec893c180d24c9046395fba4fcc31a6
CaioMassola <caio.massola@outlook.com>
2022-04-16 19:16:00 -0300

    implements: dark mode

--- main.js
+++ main.js
@@ -1,16 +1,46 @@
+var html = document.querySelector("html")
+var getStyles = (element, style) => window.getComputedStyle(element).getPropertyValue(style)
+
+var initialColors = {
+    bg: getStyles(html, "--bg"),
+    colorText: getStyles(html, "--color-text"),
+    colorHover: getStyles(html, "--color-hover"),
+    name: 'light'
+}
+
+var darkMode = {
+    bg: "#202125",
+    colorText: "#ffffff",
+    colorHover: "#38CADE",
+    name: 'dark'
+}
+
+
 function theme() {
 
     var theme = document.getElementById("theme_1").innerHTML;
-
-
-    console.log(theme)
-
+    
     if (theme == 'dark_mode') {
         theme = document.getElementById("theme_1").innerHTML = "light_mode";
         theme = document.getElementById("theme_2").innerHTML = "light_mode";
+
+        changeColors(darkMode, html)      
     } else {
         theme = document.getElementById("theme_1").innerHTML = "dark_mode";
         theme = document.getElementById("theme_2").innerHTML = "dark_mode";
+        changeColors(initialColors, html)
     }
+
+}
+
+function transformKey(key) {
+    return "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+}
+
+function changeColors (colors, html) {
+
+    Object.keys(colors).map(key => html.style.setProperty(transformKey(key), colors[key]))
+
+    console.log(colors.bg)
 
 }
